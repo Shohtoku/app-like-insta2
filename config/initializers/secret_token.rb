a967f987329a9182f9de6f8238922ddb34a859c0
shohtoku <syou0107@gmail.com>
2015-08-01 19:49:09 +0900

    Improve the README file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppLikeInsta2::Application.config.secret_key_base = 'dec5b3489908c97eed85ad4b0a1fe73008298a2421c6bdd96333410c4436afa4d91ef5d5e400fc445fa2837867164d8e0bb0b5d50bfcbb503f309d5bbbf74c6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AppLikeInsta2::Application.config.session_store :cookie_store, key: '_app-like-insta2_session'
